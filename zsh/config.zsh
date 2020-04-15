1628f632c838dca6961c9c5d6c9feb3be6c9096d
Mateus Revoredo <mmatosrevoredo@wayfair.com>
2020-04-15 18:57:47 +0200

    Separate mixed statements into respective files

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,32 +1,21 @@
 # set some history options
-setopt append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_all_dups
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_reduce_blanks
-setopt hist_save_no_dups
-setopt hist_verify
+setopt APPEND_HISTORY
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_VERIFY
 setopt INC_APPEND_HISTORY
 unsetopt HIST_BEEP
 
 # Share your history across all your terminal windows
-setopt share_history
-
+setopt SHARE_HISTORY
 
 # why would you type 'cd dir' if you could just type 'dir'?
 setopt AUTO_CD
 
 # 10 second wait if you do something that will delete everything.  I wish I'd had this before...
 setopt RM_STAR_WAIT
-
-# Keep a ton of history. You can reset these without editing .zshrc by
-# adding a file to ~/.zshrc.d.
-HISTSIZE=100000
-SAVEHIST=100000
-HISTFILE=~/.zsh_history
-export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
-
-export EDITOR="vim"
-export TERM="xterm-256color"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,9 +1,8 @@
-source ~/.dotfiles/zplug/init.zsh
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug "trapd00r/LS_COLORS"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zdharma/zsh-diff-so-fancy", as:command, use:"bin/"
+zplug "zdharma/zsh-diff-so-fancy"
 zplug "rupa/z", use:z.sh
+zplug "zpm-zsh/dircolors-material", as:plugin
 
 zplug "junegunn/fzf-bin", \
     from:gh-r, \
@@ -11,25 +10,6 @@ zplug "junegunn/fzf-bin", \
     rename-to:fzf, \
     use:"*darwin*amd64*"
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
+zplug "sharkdp/fd", as:command, from:gh-r, rename-to:fd, use:"*darwin*"
 
-zplug romkatv/powerlevel10k, as:theme, depth:1
-
-zplug load
-
-source ~/.local/share/lscolors.sh
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-# Declare the variable
-typeset -A ZSH_HIGHLIGHT_PATTERNS
-
-# To have commands starting with `rm -rf` in red:
-ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
-ZSH_HIGHLIGHT_PATTERNS+=('rm \*' 'fg=white,bold,bg=red')
-
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --no-height --no-reverse"
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git --color=always"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--ansi"
+zplug romkatv/powerlevel10k, as:theme
