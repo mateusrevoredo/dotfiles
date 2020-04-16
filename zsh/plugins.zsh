b98bd6776b75e435af563f62fe309752bfc4f3df
Mateus Revoredo <mateus.revoredo@gmail.com>
2020-04-17 00:40:40 +0200

    Move preload to separate file

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,9 +1,7 @@
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zdharma/zsh-diff-so-fancy"
 zplug "rupa/z", use:z.sh
 zplug "zpm-zsh/dircolors-material", as:plugin
-
 zplug "junegunn/fzf-bin", \
     from:gh-r, \
     as:command, \
@@ -11,5 +9,11 @@ zplug "junegunn/fzf-bin", \
     use:"*darwin*amd64*"
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
 zplug "sharkdp/fd", as:command, from:gh-r, rename-to:fd, use:"*darwin*"
+
+# zsh users
+zplug "zsh-users/zsh-completions",              defer:0
+zplug "zsh-users/zsh-autosuggestions",          defer:2, on:"zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting",      defer:3, on:"zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-history-substring-search", defer:3, on:"zsh-users/zsh-syntax-highlighting"
 
 zplug romkatv/powerlevel10k, as:theme
