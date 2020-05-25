30054766fc8bb00e80c55efb034d1f102f775b5b
Mateus Revoredo <mateus.revoredo@gmail.com>
2020-05-26 00:17:59 +0200

    Tweak macos settings

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -123,6 +123,9 @@ sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
 # Energy saving                                                               #
 ###############################################################################
 
+#System Sleep
+sudo pmset -a sleep 15
+
 # Enable lid wakeup
 sudo pmset -a lidwake 1
 
@@ -132,11 +135,11 @@ sudo pmset -a autorestart 1
 # Restart automatically if the computer freezes
 sudo systemsetup -setrestartfreeze on
 
-# Sleep the display after 15 minutes
-sudo pmset -a displaysleep 15
+# Sleep the display after 15 minutes if charging
+sudo pmset -c displaysleep 15
 
-# Disable machine sleep while charging
-sudo pmset -c sleep 0
+# Sleep the display after 15 minutes on battery
+sudo pmset -b displaysleep 5
 
 # Set machine sleep to 5 minutes on battery
 sudo pmset -b sleep 5
@@ -146,6 +149,12 @@ sudo pmset -a standbydelay 86400
 
 # Never go into computer sleep mode
 sudo systemsetup -setcomputersleep Off > /dev/null
+
+# Hibernation mode
+# 0: Disable hibernation (speeds up entering sleep mode)
+# 3: Copy RAM to disk so the system state can still be restored in case of a
+#    power failure.
+sudo pmset -a hibernatemode 0
 
 ###############################################################################
 # Screen                                                                      #
