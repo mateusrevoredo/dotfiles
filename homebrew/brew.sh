48ed951b6ee0784de6721eb64ab0c76a2571bb68
Mateus Revoredo <mateus.revoredo@gmail.com>
2020-05-25 23:42:35 +0200

    Modularize installation

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -1,3 +1,22 @@
+#!/usr/bin/env bash
+
 brew_config_dir=`dirname $0`
 #Call the other script
-brew bundle --no-lock --file=$brew_config_dir/Brewfile --no-lock
+
+for i in "$@"
+do
+case $i in
+    -a|--apps)
+    brew bundle --no-lock --file=$brew_config_dir/apps/Brewfile --no-lock
+    ;;
+    -b|--binaries)
+    brew bundle --no-lock --file=$brew_config_dir/binaries/Brewfile --no-lock
+    ;;
+    -m|--mas)
+    brew bundle --no-lock --file=$brew_config_dir/mas/Brewfile --no-lock
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
