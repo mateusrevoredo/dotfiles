f8eda8c22d00d188014239c47cc76ccb9d7d3a73
Mateus Revoredo <1175432+mateusrevoredo@users.noreply.github.com>
2020-06-10 00:52:11 +0200

    add tmux

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# ~/.macos — https://mths.be/macos
-
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
