a1abb1b5a63b13cb1f2f5d17b39c433c2c48e4f2
Mateus Revoredo <1175432+mateusrevoredo@users.noreply.github.com>
2020-06-14 22:44:52 +0200

    move asdf dependencies to its own dedicated array

--- zsh/01_path.zsh
+++ zsh/01_path.zsh
@@ -4,8 +4,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   path=(
     #Homebrew
     /usr/local/bin
-    # GNU File, Shell, and Text utilities
-    # /usr/local/opt/coreutils/libexec/gnubin
     #GNU Bison (Necessary on macOS for https://github.com/asdf-community/asdf-php
     /usr/local/opt/bison/bin
     # System Path
